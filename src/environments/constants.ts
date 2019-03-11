d35f6c4f90fd92ff45e64f6f7abaca6e94cd1ad4
luis <luiscarvajal2693@gmail.com>
2019-03-11 10:55:15 -0400

    change uri login 3

--- src/environments/constants.ts
+++ src/environments/constants.ts
@@ -1,4 +1,5 @@
 export const constants = {
   // API_PATH: 'http://wslifeusf.claropr.com/proccess/procesos2.aspx'
-  API_PATH: 'http://wslife00042ws.claroinfo.com/proccess/procesos2.aspx'
+  // API_PATH: 'http://wslife00042ws.claroinfo.com/proccess/procesos2.aspx'
+  API_PATH: 'http://usflifeline.claroinfo.com/proccess/procesos2.aspx'
 };
