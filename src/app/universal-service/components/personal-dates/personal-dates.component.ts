e6e17f93891c130a0134f94d6fc63771513fc52b
Julio Vinachi <julio899@gmail.com>
2019-03-12 13:51:01 -0400

    ajustando validacion para SSN XXX-XX-XXXX guardando digitos en variable temporal

--- src/app/universal-service/components/personal-dates/personal-dates.component.ts
+++ src/app/universal-service/components/personal-dates/personal-dates.component.ts
@@ -26,6 +26,8 @@ export interface Model {
   styleUrls: ['./personal-dates.component.scss']
 })
 export class PersonalDatesComponent implements OnInit {
+  checkSSN = false;
+  valueSSN = '';
   processValidationSIF = false;
   format2 = 'XXX-XX-XXXX';
 
@@ -47,79 +49,39 @@ export class PersonalDatesComponent implements OnInit {
     liveWithAnoterAdult: boolean;
     hasLifelineTheAdult: boolean;
     sharedMoneyWithTheAdult: boolean;
-  };
+  }();
 
-  constructor(private authenticationService: AuthenticationService, private router: Router, private fb: FormBuilder) { }
+  constructor(private authenticationService: AuthenticationService, private router: Router, private fb: FormBuilder) {}
 
   ngOnInit() {
     window.scroll(0, 0);
 
     this.form = this.fb.group({
-      sufix: [
-        null,
-        Validators.compose([
-          Validators.required
-        ])
-      ],
-      firstName: [
-        null,
-        Validators.compose([
-          Validators.required
-        ])
-      ],
+      sufix: [null, Validators.compose([Validators.required])],
+      firstName: [null, Validators.compose([Validators.required])],
       secondName: [
         null,
         Validators.compose([
           // Validators.required
         ])
       ],
-      lastName: [
-        null,
-        Validators.compose([
-          Validators.required
-        ])
-      ],
-      socialSecure: [
-        null,
-        Validators.compose([
-          Validators.required
-        ])
-      ],
-      birthday: [
-        null,
-        Validators.compose([
-          Validators.required
-        ])
-      ],
-      gender: [
-        null,
-        Validators.compose([
-          Validators.required
-        ])
-      ],
-      idType: [
-        null,
-        Validators.compose([
-          Validators.required
-        ])
-      ],
-      idNumber: [
-        null,
-        Validators.compose([
-          Validators.required
-        ])
-      ],
-      idExpirationDate: [
-        null,
-        Validators.compose([
-          Validators.required
-        ])
-      ]
+      lastName: [null, Validators.compose([Validators.required])],
+      socialSecure: [null, Validators.compose([Validators.required])],
+      birthday: [null, Validators.compose([Validators.required])],
+      gender: [null, Validators.compose([Validators.required])],
+      idType: [null, Validators.compose([Validators.required])],
+      idNumber: [null, Validators.compose([Validators.required])],
+      idExpirationDate: [null, Validators.compose([Validators.required])]
     });
   }
 
   goToSocialSecureVerification() {
-    if (this.form.valid && (this.model.sharedMoneyWithTheAdult === false || this.model.hasLifelineTheAdult === false || this.model.liveWithAnoterAdult === false)) {
+    if (
+      this.form.valid &&
+      (this.model.sharedMoneyWithTheAdult === false ||
+        this.model.hasLifelineTheAdult === false ||
+        this.model.liveWithAnoterAdult === false)
+    ) {
       console.log(this.model);
       this.processValidationSIF = true;
 
@@ -134,11 +96,11 @@ export class PersonalDatesComponent implements OnInit {
   }
 
   checkNumbersOnly(event: any): boolean {
-   return Util.checkNumbersOnly(event);
+    return Util.checkNumbersOnly(event);
   }
 
   checkCharactersOnly(event: any): boolean {
-   return Util.checkCharactersOnly(event);
+    return Util.checkCharactersOnly(event);
   }
 
   formatInputSocialSecure(input: string) {
@@ -146,16 +108,37 @@ export class PersonalDatesComponent implements OnInit {
   }
 
   public formatInput(input: string, format: string) {
+    // Almacenando valor real en variable temporal
+    if (input.length > 1 && input.substr(input.length - 1, 1) !== 'X') {
+      console.log(input.substr(input.length - 1, 1));
+      this.valueSSN += String(input.substr(input.length - 1, 1));
+    } else {
+      if (input !== 'X') {
+        this.valueSSN = input;
+        console.log(input);
+      }
+    }
+    // Si llega a 11 Caracteres hay que hacer la validacion Real contra el servicio
+    // ya que tiene el formato XXX-XX-XXXX  de 11 caracteres serian 9 digitos
+    if (input.length === 11) {
+      this.checkSSN = true;
+    }
+
     if (format === this.format2) {
       if (input.length === 4) {
-        return input.substr(0, input.length - 1) + '-' + input.substr(input.length - 1, input.length).replace(/[0-9]/g, "X");
+        return (
+          input.substr(0, input.length - 1) + '-' + input.substr(input.length - 1, input.length).replace(/[0-9]/g, 'X')
+        );
       }
 
       if (input.length === 7) {
-        return input.substr(0, input.length - 1) + '-' + input.substr(input.length - 1, input.length).replace(/[0-9]/g, "X");
+        console.log(input);
+        return (
+          input.substr(0, input.length - 1) + '-' + input.substr(input.length - 1, input.length).replace(/[0-9]/g, 'X')
+        );
       }
 
-      return input.replace(/[0-9]/g, "X");
+      return input.replace(/[0-9]/g, 'X');
     }
 
     return '';
