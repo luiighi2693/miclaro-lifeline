c67d6382dbe35787f70dced15341c6af11102c6a
Julio Vinachi <julio899@gmail.com>
2019-04-30 10:35:12 -0400

    cambio en url en constants

--- src/environments/constants.ts
+++ src/environments/constants.ts
@@ -2,5 +2,5 @@ export const constants = {
   // API_PATH: 'http://wslifeusf.claropr.com/proccess/procesos2.aspx'
   // API_PATH: 'http://wslife00042ws.claroinfo.com/proccess/procesos2.aspx'
   API_PATH: 'http://usflifeline.claroinfo.com/proccess/procesos2.aspx',
-  URL_CASES: 'http://wslifeusf.claropr.com/Service/svc/1/reports/getCasesWithFilters.MCAPI'
+  URL_CASES: 'http://wslife00042ws.claroinfo.com/Service/svc/1/reports/getCasesWithFilters.MCAPI'
 };
