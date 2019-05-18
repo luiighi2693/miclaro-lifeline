9f9401a61ea0bb26ea277b19ede8f145aef218ba
Julio Vinachi <julio899@gmail.com>
2019-05-18 19:22:56 -0400

    firma fluida

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,10 +14,12 @@ export class HomeComponent implements OnInit {
   isLoading: boolean;
   // passed through to szimek/signature_pad constructor
   signaturePadOptions: Object = {
-    minWidth: 5,
+    minWidth: 0.1, // (float) Minimum width of a line. Defaults to 0.5
+    maxWidth: 0.3, // (float) Maximum width of a line. Defaults to 2.5
     canvasWidth: 740,
     canvasHeight: 180
   };
+  // throttle: 75,  // (integer) Draw the next point at most once per every x milliseconds. Set it to 0 to turn off throttling. Defaults to 16
 
   constructor(private router: Router, private authenticationService: AuthenticationService) {
     authenticationService.validaSessionActiva();
@@ -43,7 +45,8 @@ export class HomeComponent implements OnInit {
   // tslint:disable-next-line: use-life-cycle-interface
   ngAfterViewInit() {
     // this.signaturePad is now available
-    this.signaturePad.set('minWidth', 5); // set szimek/signature_pad options at runtime
+    this.signaturePad.set('minWidth', 0.5); // set szimek/signature_pad options at runtime
+    this.signaturePad.set('maxWidth', 3);
     this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
   }
 
@@ -51,6 +54,7 @@ export class HomeComponent implements OnInit {
     // will be notified of szimek/signature_pad's onEnd event
     const firmaUrl = this.signaturePad.toDataURL();
     console.log(firmaUrl);
+    console.log(this.signaturePad);
   }
 
   drawCompleteSave() {
@@ -64,6 +68,7 @@ export class HomeComponent implements OnInit {
   drawStart() {
     // will be notified of szimek/signature_pad's onBegin event
     console.log('begin drawing');
+    console.log(this.signaturePad);
   }
   copyCode(code: any) {
     const aux = document.createElement('input');
