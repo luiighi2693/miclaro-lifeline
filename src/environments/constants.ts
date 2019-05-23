b004d4d0efb24c425a7c04d63e71e5a5d74ba600
luis <luiscarvajal2693@gmail.com>
2019-05-23 13:27:07 -0400

    change endpoint prod

--- src/environments/constants.ts
+++ src/environments/constants.ts
@@ -1,5 +1,6 @@
 export const constants = {
-  API_PATH: 'http://usflifeline.claroinfo.com/proccess/procesos2.aspx',
+  API_PATH: 'https://wslife00042ws.claroinfo.com/proccess/procesos2.aspx',
+  // API_PATH: 'http://usflifeline.claroinfo.com/proccess/procesos2.aspx',
   MOCK_API_PATH: 'https://ddec704a-e29f-4d45-a43e-8abebe7df566.mock.pstmn.io/proccess/procesos2.aspx',
   MOCK_API: false
 };
