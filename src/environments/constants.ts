ec3ac08cb97f7239e2a4eaa8b88dbf9711a13e3d
Julio Vinachi <julio899@gmail.com>
2019-06-12 16:30:17 -0400

    control y carga de filtro por numero de casos en tabla

--- src/environments/constants.ts
+++ src/environments/constants.ts
@@ -3,5 +3,5 @@ export const constants = {
   // API_PATH: 'http://usflifeline.claroinfo.com/proccess/procesos2.aspx',
   // MOCK_API_PATH: 'https://ddec704a-e29f-4d45-a43e-8abebe7df566.mock.pstmn.io/proccess/procesos2.aspx',
   MOCK_API_PATH: 'http://localhost/api-claro-usf/',
-  MOCK_API: true
+  MOCK_API: false
 };
